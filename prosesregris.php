0b77867e38d794cdcf2f9b37e364e7495b032736
Fajar Febbi Aldianto <43521209+fajarfaldianto18@users.noreply.github.com>
2018-10-05 10:12:23 +0700

    prosesregris.php
    fajar febbi aldianto
    6701174130
    d3mi 41 04

--- prosesregris.php
+++ prosesregris.php
@@ -1,4 +1,24 @@
-<?php
+if (isset($_POST['kirim'])) {
+		$nim = $_POST['nim'];
+		$nama = $_POST['nama'];
+		$email = $_POST['email'];
 
-
+		if (strlen($nim)==10) {
+				if (strlen($nama)<25) {
+					if (strpos($email, "@gmail.com")) {
+						$qry = mysqli_query($conn, "INSERT INTO `data_mhs`(`nim`, `nama`, `email`)  
+									VALUES  ('$nim', '$nama', '$email')");
+						
+						echo "---Berhasil----";
+					}else{
+						echo "-----Gagal-----";
+					}
+			}else{
+				echo "-----Gagal------";
+			}
+			}else{
+				echo "------Gagal------";
+			}
+	
+	}		
 ?>
